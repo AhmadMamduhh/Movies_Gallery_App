dc4322ae4a885cb5f258e84807adad6e30c48136
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-12 23:50:07 +0200

    Fixed a bug in the Movie model data class.

--- app/src/main/java/com/example/moviesapp/models/MovieResponse.kt
+++ app/src/main/java/com/example/moviesapp/models/MovieResponse.kt
@@ -4,7 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(var results : List<Movie>)
 
-data class Movie(var title : String, var poster : String,
+data class Movie(var title : String,
                  var overview: String,
+                 @SerializedName("poster_path") var posterPath : String,
                  @SerializedName("release_date") var releaseDate : String,
                  @SerializedName("vote_average") var voteAverage : String)
